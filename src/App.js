d632cc3f1385d04961554f7dac74fe1437bd69ef
Abu thakir <abudx.at@gmail.com>
2022-11-29 10:59:08 +0530

    spell check task

--- src/App.js
+++ src/App.js
@@ -1,27 +1,92 @@
-import React from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import './App.css';
-import { GrammarlyEditor } from './components/spell-check-component';
+import { useEffect } from 'react';
+import axios from 'axios';
+import { SerpApiSearch } from 'google-search-results-nodejs';
+// import myLib from "imports-loader?imports=default|jquery|$!./example.js";
+//    
 
 
 const App = () => {
-  // const dispatch = useDispatch();
-  // const counterState = useSelector((state) => state.count);
-  // const { value } = counterState;
+  const [content, setContent] = useState(0);
+  const [text, setText] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+  var textAreaText = '';
 
-  // const incCounter = () => {
-  //   let newValue = value + 1;
-  //   dispatch(updateValue(newValue));
-  // }
+  function spellCheck() {
+    textAreaText = document.querySelector('.text-area').value;
+    const options = {
+      method: 'POST',
+      url: 'https://jspell-checker.p.rapidapi.com/check',
+      headers: {
+        'content-type': 'application/json',
+        'X-RapidAPI-Key': '1ffdf83fe8mshf929b293d984f30p1fbb24jsn78a4f04aee41',
+        'X-RapidAPI-Host': 'jspell-checker.p.rapidapi.com'
+      },
+      data: {
+        "language": "enUS",
+        "fieldvalues": textAreaText,
+        "config": {
+          "forceUpperCase": false,
+          "ignoreIrregularCaps": false,
+          "ignoreFirstCaps": true,
+          "ignoreNumbers": true,
+          "ignoreUpper": false,
+          "ignoreDouble": false,
+          "ignoreWordsWithNumbers": true
+        }
+      }
+    };
 
-  // const decCounter = () => {
-  //   let newValue = value - 1;
-  //   dispatch(updateValue(newValue));
-  // }
+    axios.request(options).then(function (response) {
+      let res = response.data;
+      let { spellingErrorCount, elements } = res;
+      console.log(spellingErrorCount, elements);
+      if (elements !== undefined) {
+        setSuggestions(elements[0].errors);
+      }
+      else {
+        setSuggestions([]);
+      }
+
+      setContent(spellingErrorCount);
+      console.log(content);
+      console.log("suggestions", suggestions);
+
+    }).catch(function (error) {
+      console.error(error);
+    });
+
+  }
 
   return (
-    <div className="App">
-      <GrammarlyEditor />
-    </div>
+    <>
+      <div className='wrapper'>
+        <textarea id="checkTest" className={`text-area ${content > 0 ? "not-correct" : ""}`} placeholder='please enter text to check'>
+
+        </textarea>
+        <div className='error-count'>
+          Spellingerrorcount : {content}
+        </div>
+        <button className='submit-button' onClick={spellCheck}>
+          submit
+        </button>
+      </div>
+      {
+        suggestions.length > 0 ? suggestions.map((suggestionsData) => (
+          <div className='suggestions-section'>
+            <div className='words'>
+              word : {suggestionsData.word}
+            </div>
+            <div className='suggestions-text'>
+              suggestions: {suggestionsData.suggestions.length > 0 ? `${suggestionsData.suggestions},` : "No-suggestions"}
+            </div>
+          </div>
+        )) : ""
+      }
+    </>
+
+
   );
 
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,31 @@
-module.exports = {
-    entry: './src/index.js',
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['*', '.js', '.jsx']
-    },
-    output: {
-        path: __dirname + '/dist',
-        publicPath: '/',
-        filename: 'bundle.js'
-    },
-    devServer: {
-        contentBase: './dist'
-    },
-    mode: 'development'
-};
+// module.exports = {
+//     module: {
+//         rules: [
+//             {
+//                 // You can use `regexp`
+//                 // test: /example\.js/$
+//                 test: require.resolve("example.js"),
+//                 use: [
+//                     {
+//                         loader: "imports-loader",
+//                         options: {
+//                             imports: [
+//                                 "default jquery $",
+//                                 "default lib_2 lib_2_default",
+//                                 "named lib_3 lib2_method_1",
+//                                 "named lib_3 lib2_method_2 lib_2_method_2_short",
+//                                 "namespace lib_4 my_namespace",
+//                                 "side-effects lib_5",
+//                                 {
+//                                     syntax: "default",
+//                                     moduleName: "angular",
+//                                     name: "angular",
+//                                 },
+//                             ],
+//                         },
+//                     },
+//                 ],
+//             },
+//         ],
+//     },
+// };
